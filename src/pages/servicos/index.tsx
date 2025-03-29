27624afcf9e5b6e36c0930275cde5faaa251e0ec
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-03-29 17:38:37 -0300

    substituir seção de contato por componente FormContato e remover estilos associados

--- src/pages/servicos/index.tsx
+++ src/pages/servicos/index.tsx
@@ -1,5 +1,6 @@
 import "@/app/globals.css";
 import Footer from "@/components/footer";
+import FormContato from "@/components/form-contato";
 import Menu from "@/components/menu";
 import Head from "next/head";
 import Image from "next/image";
@@ -82,66 +83,7 @@ const Servicos = () => {
             </div>
           ))}
         </section>
-        <section className="contact-section">
-          <div className="contact-section-title">
-            <h1 className="contact-section-title-text">Entre em contato</h1>
-          </div>
-          <div className="contact-section-form">
-            <p className="contact-section-description">
-              Solicite um orçamento sem compromisso e conte com a experiência de
-              nossos especialistas.
-            </p>
-            <form
-              className="contact-form"
-              action="/api/enviar-email"
-              method="POST"
-            >
-              <div className="form-group">
-                <label htmlFor="name">Nome</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Seu nome"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">E-mail</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Seu e-mail"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="phone">Telefone</label>
-                <input
-                  type="tel"
-                  id="phone"
-                  name="phone"
-                  placeholder="Seu telefone"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="details">Envie mais detalhes</label>
-                <textarea
-                  id="details"
-                  name="details"
-                  placeholder="Escreva aqui..."
-                  rows={5}
-                  required
-                ></textarea>
-              </div>
-              <button type="submit" className="submit-button">
-                Enviar
-              </button>
-            </form>
-          </div>
-        </section>
+        <FormContato />
         <Footer />
       </div>
     </>
