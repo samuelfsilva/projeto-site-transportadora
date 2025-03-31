82df8db9762881543b46c190016b95ba0f982004
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-03-31 15:59:29 -0300

    adicionar origem permitida para desenvolvimento no arquivo de configuração do Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  allowedDevOrigins: ["http://localhost:3000"],
 };
 
 export default nextConfig;
