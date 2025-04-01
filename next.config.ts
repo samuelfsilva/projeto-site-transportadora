dd131ec729bc74894401943cb629f3d863e9b4fd
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-04-01 11:50:22 -0300

    Correção do caminho de configuração do prefixo dos arquivos estáticos

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   allowedDevOrigins: ["http://localhost:3000"],
-  //publicPath: process.env.NEXT_PUBLIC_REPOSITORY_URL,
-  basePath: process.env.NEXT_PUBLIC_REPOSITORY_URL,
+  assetPrefix: process.env.NEXT_PUBLIC_REPOSITORY_URL,
 };
 
 export default nextConfig;
