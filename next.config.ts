02253f4a6dcb7c3697a6f1cc0c14b9e3ba5518ec
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-04-01 12:02:48 -0300

    Correção de prefixo de url de arquivos estático

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   allowedDevOrigins: ["http://localhost:3000"],
-  assetPrefix: process.env.NEXT_PUBLIC_REPOSITORY_URL,
+  publicRuntimeConfig: {
+    staticFolder: process.env.NEXT_PUBLIC_REPOSITORY_URL,
+  },
 };
 
 export default nextConfig;
