42da5cf93d075a1db0521dbb5e398e6c67ebcbb9
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-04-01 14:44:02 -0300

    Inclusão de basePath

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   allowedDevOrigins: ["http://localhost:3000"],
+  basePath: "https://samuelfsilva.github.io/projeto-site-transportadora/",
 };
 
 export default nextConfig;
