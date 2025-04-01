c46aa379ccbd61bc70149416935459c888917d06
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-04-01 11:42:19 -0300

    Configurando caminho para pasta de arquivos staticos

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   allowedDevOrigins: ["http://localhost:3000"],
+  publicPath: process.env.NEXT_PUBLIC_REPOSITORY_URL,
+  //basePath: process.env.NEXT_PUBLIC_REPOSITORY_URL,
 };
 
 export default nextConfig;
