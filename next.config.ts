72786bcf91637e9cc0be011265f34ddba96a9164
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2025-04-02 16:38:19 -0300

    Configuração do next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   allowedDevOrigins: ["http://localhost:3000"],
-  basePath: "https://samuelfsilva.github.io/projeto-site-transportadora",
 };
 
 export default nextConfig;
